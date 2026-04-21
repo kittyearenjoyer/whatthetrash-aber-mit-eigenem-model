0e20c1dbf2c1826a5cb35fa58b57bb3292143ab7
KittyEarEnjoyer <juliuslindenau@gmail.com>
2026-04-21 14:48:43 +0200

    Update app.py
    img statt bid

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if hochgeladen:
 
     with col2:
         with st.spinner("Analysiere …"):
-            klasse, konfidenz, alle_preds = klassifizieren(bild)
+            klasse, konfidenz, alle_preds = klassifizieren(img)
 
         tonne_text, tonne_farbe = TONNE.get(klasse, ("❓ Unbekannt", "#999999"))
 
