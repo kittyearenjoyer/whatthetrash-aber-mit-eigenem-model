1cb77552efe5796410e67e4406449730da3ae150
KittyEarEnjoyer <juliuslindenau@gmail.com>
2026-04-21 14:20:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import numpy as np
 from PIL import Image
 import tensorflow as ts
 import os
+import tf_keras as keras  # statt tensorflow.keras
 
+
+@st.cache_resource
+def lade_modell():
+    return keras.models.load_model(MODEL_PATH)
+    
 # ── Konfiguration ──────────────────────────────────────────────────────────────
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "mein_model_für_die_app.h5")
 IMG_SIZE   = (224, 224)          # anpassen falls dein Modell eine andere Größe erwartet
